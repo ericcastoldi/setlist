a33236c26ec639658c3ceab5aa239d9f1e1bd184
Eric Castoldi <eric.castoldi@gmail.com>
2021-03-05 23:06:58 -0300

    Client ID e Client Secret via env

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
 import base64
 import requests
 
+
+
 def spotify_auth():
-    client_id = '2a37ebbb434e4ce09b406d0d34b9ca9a'
-    client_secret = '742dc8ae817044e7b5bd66393a524e62'
+    client_id = os.environ['SPOTIFY_CLIENT_ID']
+    client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
     client_credentials = f'{client_id}:{client_secret}'
     encoded_credentials = base64.b64encode(client_credentials.encode())
 
